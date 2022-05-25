020080dae8587e860b439cf715551c881b686aa6
MarshallMathers1313 <AshurovAbdulla13>
2022-05-25 15:50:29 +0500

    Добавьте файлы проекта.

---
+++ lib.cpp
@@ -0,0 +1,61 @@
+﻿
+#include <iostream>
+#include<string>
+using namespace std;
+// Создать класс библиотека
+// Классы жанры
+// Класс книга
+// Класс авторы
+class lib
+{
+    zhanr* n;
+public:
+    lib()
+    {
+
+    }
+
+};
+class zhanr
+{
+    book* Book;
+public:
+    zhanr()
+    {
+
+    }
+
+};
+class book
+{
+    string name;
+    int publish;
+    author* auth;
+public:
+    book()
+    {
+
+    }
+    ~book()
+    {
+        delete [] auth;
+   }
+
+};
+class author
+{
+    string name;
+    string surname;
+    string old;
+public:
+    author()
+    {
+
+    }
+    
+};
+int main()
+{
+    
+}
+
