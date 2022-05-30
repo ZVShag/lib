d4918eae393253dbc82bb2a7ad31287a5dea87d5
MarshallMathers1313 <AshurovAbdulla13>
2022-05-30 18:46:27 +0500

    final30.05.2022

--- lib.cpp
+++ lib.cpp
@@ -16,7 +16,8 @@ using namespace std;
 //      d) по точной дате публикации или периоду
 // 4. Редактировать информации о книгах
 // 5. Удалять книги из библиотеки
-
+int countzhanr = 0;
+int n = 0;
 string Input(string mess)
 {
     string str;
@@ -34,6 +35,25 @@ string Input(string mess)
     }
     return str;
 
+}
+int Num_input(string mess)
+{
+    int k = 0;
+    cout << mess << endl;
+    while (true)
+    {
+        try
+        {
+            cin >> k;
+            return true;
+            break;
+
+        }
+        catch (...)
+        {
+            cout << mess << endl;
+        }
+    }
 }
 
 
@@ -71,19 +91,19 @@ class book
     int publish;
     author *auth;
 public:
+   
     book()
     {
+        int acount = 0;
         this->name = Input("Name of book:");
-        cout << "Date of publish: " << endl;
-        cin >> this->publish;
-        cout << "Enter author count:" << endl;
-        int n = 0;
-        cin >> n;
-        this->auth = new author[n];
-        for (int i = 0; i < n; i++)
+        this->publish=Num_input("Date of publish: ");
+        acount = Num_input("Enter author count:");
+        this->auth = new author[acount];
+        for (int i = 0; i < acount; i++)
         {
             auth[i];
         }
+        n++;
 
     }
     string Get_name() { return this->name; }
@@ -103,9 +123,8 @@ public:
     zhanr()
     {
         this->name = Input("Name of zhanr:");
-        this->n = 0;
-        cout << "Enter count of book: " << endl;
-        cin >> this->n;
+        countzhanr++;
+        this->n = Num_input("Enter count of book: ");
         this->Book = new book[n];
         for (int i = 0; i < n; i++)
         {
@@ -116,29 +135,31 @@ public:
     {
         return this->name;
     }
-    bool Get_element(string name)
+    bool Get_element_name(string name)
     {
         for (int i = 0; i < n; i++)
         {
             if (Book[i].Get_name() == name)
+            {
                 return true;
+            }
         }
     }
 };
 class lib
 {
     zhanr* a;
-    int n;
+  
 public:
     lib()
     {
-        this->a = new zhanr[5];
+        this->a = new zhanr[1];
     }
     void Serach_name_book(string sname)
     {
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < countzhanr; i++)
         {
-            if (a[i].Get_element(sname))
+            if (a[i].Get_element_name(sname))
                 cout << "Ok";
 
         }
@@ -149,6 +170,6 @@ public:
 
 int main()
 {
-    author Pushkin;
+    book Pushkin;
 }
 
